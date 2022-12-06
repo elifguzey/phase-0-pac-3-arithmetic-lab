e665850be61dbd6cdd2e2e992289e12f74bdb634
Elif Guzey <elifguzey3@gmail.com>
2022-12-05 22:41:46 -0500

    commit tree 808459e21b8eb227eb672360be2f2cef592af6ae

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(x , y) {
+return x+y ;    
+}
+function subtract (x,y) {
+return x-y ;
+}
+function multiply (x,y) {
+    return x*y ;
+}
+function divide (x,y) {
+    return x/y ;
+}
+function increment (x) {
+    return ++ x
+}
+function decrement (x) {
+    return -- x
+}
+function makeInt (n) {
+    return parseInt(n,10)
+}
+function preserveDecimal (n) {
+    return parseFloat (n,10)
+} 
